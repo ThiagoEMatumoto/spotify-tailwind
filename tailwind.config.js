66bd967baf6cc24b731a6c87ee64bbea22af5471
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-04-20 14:04:43 -0300

    Adicionando o corpo do site

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,13 @@ module.exports = {
         '125px': '125px'
       },
       colors: {
-        'hoverspt': '#18d760',
-        'purple-main': '#ed46b9',
-        'green-main': '#1ed760',
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
       },
       backgroundImage: theme => ({
-        'spotify-theme': "url('/img/bursts.svg')",
-        'spotify-theme-mobile': "url('/img/bursts-mobile.svg')",
+        'spotify-theme': "url('/src/img/bursts.svg')",
+        'spotify-theme-mobile': "url('/src/img/bursts-mobile.svg')"
       }),
       backgroundSize: {
         '175%': '175%',
@@ -22,7 +22,7 @@ module.exports = {
       },
       backgroundPosition: {
         'banner': '46% 4%',
-        'banner-mobile': 'top 25% center',
+        'banner-mobile': 'top 25% center'
       },
       fontSize: {
         '9xl': '9rem'
